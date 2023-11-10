2f6c92ecf207d5fd4482d64e1d5f3c171bf6d419
abhishek <abhishektupsundar1@gmail.com>
2023-11-10 12:36:41 +0530

    Perfect working

---
+++ App.js
@@ -0,0 +1,17 @@
+const emoji = document.querySelector('#emoji')
+
+const emojis=[
+    "😊",
+    "❤",
+    "😁",
+    "💕",
+    "😘",
+    "😎",
+    "😍",
+    "🥰"
+];
+
+emoji.addEventListener('mouseover',()=>
+{
+    emoji.innerHTML=emojis[Math.floor(Math.random() * emojis.length)]
+})
